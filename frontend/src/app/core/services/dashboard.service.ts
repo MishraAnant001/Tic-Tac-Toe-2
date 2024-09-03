b8f14182a1937c561261673adea984fd7aa68d46
MishraAnant001 <87534143+MishraAnant001@users.noreply.github.com>
2024-09-03 10:02:14 +0530

    Updated backend url

--- frontend/src/app/core/services/dashboard.service.ts
+++ frontend/src/app/core/services/dashboard.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class DashboardService {
-  private adminApi = "http://192.168.4.5:8000/api/v1/dashboard/admin"
+  private adminApi = "https://5cg4v22c-8000.inc1.devtunnels.ms:8000/api/v1/dashboard/admin"
   constructor(private http:HttpClient) { }
 
   getAdminData(){
