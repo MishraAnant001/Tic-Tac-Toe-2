5557150dbef48d25afac07da28acca9b17a03dfd
MishraAnant001 <87534143+MishraAnant001@users.noreply.github.com>
2024-09-03 10:10:02 +0530

    Update user.service.ts

--- frontend/src/app/core/services/user.service.ts
+++ frontend/src/app/core/services/user.service.ts
@@ -6,9 +6,9 @@ import { IUser } from 'src/app/models';
   providedIn: 'root'
 })
 export class UserService {
-  private signupApi = "https://5cg4v22c-8000.inc1.devtunnels.ms:8000/api/v1/user/signup"
-  private loginApi = "https://5cg4v22c-8000.inc1.devtunnels.ms:8000/api/v1/user/login"
-  private getApi = "https://5cg4v22c-8000.inc1.devtunnels.ms:8000/api/v1/user/"
+  private signupApi = "https://5cg4v22c-8000.inc1.devtunnels.ms/api/v1/user/signup"
+  private loginApi = "https://5cg4v22c-8000.inc1.devtunnels.ms/api/v1/user/login"
+  private getApi = "https://5cg4v22c-8000.inc1.devtunnels.ms/api/v1/user/"
   constructor(private http: HttpClient) { }
 
   signupUser(data: IUser) {
